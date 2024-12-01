7b63d2e3831da44c762eeae6fcee33fde218c1d0
mubashar-mizA <mirzamubashar535@gmail.com>
2024-12-01 08:37:05 +0500

    Adding the route for deleted todos in main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Landing from './components/landing.jsx'
 import TodoDashboard from './components/todo.dashboard.jsx'
 import TodoList from './components/todoList.jsx'
+import DeletedTodos from './components/deletedTodos.jsx'
 
 
 
@@ -16,6 +17,7 @@ createRoot(document.getElementById('root')).render(
         <Route path='/' element={<Landing />} />
         <Route path='/todo-dashboard' element={<TodoDashboard />} />
         <Route path='/todo-dashboard/todo-list' element={<TodoList />} />
+        <Route path='/todo-dashboard/todo-deleted' element={<DeletedTodos />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
