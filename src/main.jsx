ebda61f8429160a47bed172bb8f5c14de0bbb083
mubashar-mizA <mirzamubashar535@gmail.com>
2024-12-04 12:40:35 +0500

    provide the app store configs

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { store } from './features/store.js'
+import { Provider } from 'react-redux'
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Landing from './components/landing.jsx'
@@ -12,13 +14,16 @@ import DeletedTodos from './components/deletedTodos.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Landing />} />
-        <Route path='/todo-dashboard' element={<TodoDashboard />} />
-        <Route path='/todo-dashboard/todo-list' element={<TodoList />} />
-        <Route path='/todo-dashboard/todo-deleted' element={<DeletedTodos />} />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>,
+    <Provider store={store}>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Landing />} />
+          <Route path='/todo-dashboard' element={<TodoDashboard />} />
+          <Route path='/todo-dashboard/todo-list' element={<TodoList />} />
+          <Route path='/todo-dashboard/todo-deleted' element={<DeletedTodos />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  </StrictMode >,
 )
