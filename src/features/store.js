a654927eca71e695e3aefeaad358d7d09349b68f
mubashar-mizA <mirzamubashar535@gmail.com>
2024-12-07 08:30:08 +0500

    config the store

--- src/features/store.js
+++ src/features/store.js
@@ -1,7 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import todoReducer from './todoslice.js'
+
+import todoReducer from './todo.slice.js'
+import authReducer from './auth.slice.js'
+
+
 export const store = configureStore({
-    reducer:{
-        todos:todoReducer
-    } 
+    reducer: {
+        todos: todoReducer,
+        auth: authReducer
+    }
 })
