ac084a62e9f81e4b50773406cd2c6692eb437892
mubashar-mizA <mirzamubashar535@gmail.com>
2024-12-07 08:31:44 +0500

    providing the store to the whole app

--- src/main.jsx
+++ src/main.jsx
@@ -9,21 +9,25 @@ import Landing from './components/landing.jsx'
 import TodoDashboard from './components/todo.dashboard.jsx'
 import TodoList from './components/todoList.jsx'
 import DeletedTodos from './components/deletedTodos.jsx'
-
-
+import Signup from './components/signup.jsx'
+import Signin from './components/signin.jsx'
+import ProtectedRoute from './components/protectedRoutes.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Landing />} />
-          <Route path='/todo-dashboard' element={<TodoDashboard />} />
-          <Route path='/todo-dashboard/todo-list' element={<TodoList />} />
-          <Route path='/todo-dashboard/todo-deleted' element={<DeletedTodos />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/signin' element={<Signin />} />
+
+          {/* Protected Routes */}
+          <Route path='/todo-dashboard' element={<ProtectedRoute element={<TodoDashboard />} />} />
+          <Route path='/todo-dashboard/todo-list' element={<ProtectedRoute element={<TodoList />} />} />
+          <Route path='/todo-dashboard/todo-deleted' element={<ProtectedRoute element={<DeletedTodos />} />} />
         </Routes>
       </BrowserRouter>
     </Provider>
-  </StrictMode >,
+  </StrictMode>
 )
